317e03474f484ff5cdd157fb47d59818a7f34ecd
ChengZe <1577403684@qq.com>
2021-11-08 14:35:36 +0800

    观察者模式

--- src/main/java/com/example/demopatterns/observer/Subject.java
+++ src/main/java/com/example/demopatterns/observer/Subject.java
@@ -5,15 +5,35 @@ import java.util.List;
 
 /**
  * 目标接口
+ *
  * @author ChengZe
  */
 public interface Subject {
+    /**
+     * 观察者
+     */
+    List<Observer> observers = new ArrayList<>();
 
-    List<Observer> observers=new ArrayList<>();
-
+    /**
+     * 添加
+     *
+     * @param observer
+     * @return
+     */
     int attach(Observer observer);
 
+    /**
+     * 移除
+     *
+     * @param observer
+     * @return
+     */
     int detach(Observer observer);
 
+    /**
+     * 通知
+     *
+     * @return
+     */
     int nottify();
 }
